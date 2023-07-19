54605b8a7cd6a90941b5aa8fed9e83a8e9df5051
Felipe <felipemoraespalomo@gmail.com>
2023-07-19 18:12:09 -0300

    exercicio ajax

---
+++ exercicio_ajax/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    const nameElement = $('#name');
+    const usernameElement = $('#username');
+    const avatarElement = $('#avatar');
+    const reposElement = $('#repos');
+    const followersElement = $('#followers');
+    const followingElement = $('#following');
+    const linkElement = $('#link');
+
+    $.ajax('https://api.github.com/users/FelipeMoraesPalomo')
+    .done(function(json){
+        nameElement.text(json.name)
+        usernameElement.text(json.login)
+        avatarElement.attr('src', json.avatar_url)
+        followingElement.text(json.following)
+        followersElement.text(json.followers)
+        reposElement.text(json.public_repos)
+        linkElement.attr('href', json.html_url)
+    })
+})
