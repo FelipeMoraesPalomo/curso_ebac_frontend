cd8b2a1f1b8f13a0df8553b3af0db0cd539a7635
Felipe <felipemoraespalomo@gmail.com>
2023-08-25 11:44:22 -0300

    exerciciots

---
+++ ex_m26/src/funcoes.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(n1, n2) {
+    return n1 * n2;
+}
+function saudação(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplica(2, 8));
+console.log(saudação('Felipe'));
---
+++ ex_m26/src/funcoes.ts
@@ -0,0 +1,11 @@
+function multiplica(n1:number, n2:number){
+    return n1 * n2
+}
+
+function saudação(nome:string){
+    return "Olá " + nome
+}
+
+console.log(multiplica(2, 8))
+
+console.log(saudação('Felipe'))
